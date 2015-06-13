c57db7ea114b06acab9d54a93dc64dd7be82d79e
Mohammad Nurdin <nurdinnorazanservices@gmail.com>
2015-06-13 12:19:21 +0800

    Initial Commit

---
+++ Foodo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Foodo
+//
+//  Created by Mohammad Nurdin bin Norazan on 6/13/15.
+//  Copyright (c) 2015 AHKL2015. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
