46b87cff8c598a3d004bbf35c40ed73b3867d91d
Mohammad Nurdin <nurdinnorazanservices@gmail.com>
2015-06-13 21:41:53 +0800

    Update Validation class.

---
+++ Foodo/ValidationViewController.swift
@@ -0,0 +1,33 @@
+
+//
+//  ValidationViewController.swift
+//  Foodo
+//
+//  Created by Mohammad Nurdin bin Norazan on 6/13/15.
+//  Copyright (c) 2015 AHKL2015. All rights reserved.
+//
+
+import UIKit
+
+class ValidationViewController: UIViewController {
+
+    @IBOutlet weak var txtOrganization: UITextField!
+    @IBOutlet weak var txtPhone: UITextField!
+    @IBOutlet weak var txtEmail: UITextField!
+    @IBOutlet weak var btnValidation: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        btnValidation.backgroundColor = UIColor.clearColor()
+        btnValidation.tintColor = UIColor.whiteColor()
+        btnValidation.layer.cornerRadius = 5
+        btnValidation.layer.borderWidth = 1
+        btnValidation.layer.borderColor = UIColor.grayColor().CGColor
+        btnValidation.layer.backgroundColor = UIColor.orangeColor().CGColor
+        
+    }
+
+}
